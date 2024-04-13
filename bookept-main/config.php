81ff309aa22ef55f05a91c0f2a52362f60c93468
dien-tran <haodientran04@gmail.com>
2024-04-13 23:44:37 +0700

    Test check
    kiểm tra phần source code

---
+++ bookept-main/config.php
@@ -0,0 +1,7 @@
+<?php
+//Development
+// $conn = mysqli_connect('localhost:3325', 'root', '', 'shop_db') or die('connection failed');
+//Production
+$conn = mysqli_connect("127.0.0.1", "root","", "test") or die('connection failed');
+
+?>
