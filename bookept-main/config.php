88d8eb701ec587a7ced6a67b81cf518b3ed7fd01
dien-tran <haodientran04@gmail.com>
2024-04-26 15:36:14 +0700

    Update config.php
    chỉnh lại tên database trên php của Điền

--- bookept-main/config.php
+++ bookept-main/config.php
@@ -2,6 +2,6 @@
 //Development
 // $conn = mysqli_connect('localhost:3325', 'root', '', 'shop_db') or die('connection failed');
 //Production
-$conn = mysqli_connect("127.0.0.1", "root","", "shop_db") or die('connection failed');
+$conn = mysqli_connect("127.0.0.1", "root","", "test") or die('connection failed');
 
 ?>
